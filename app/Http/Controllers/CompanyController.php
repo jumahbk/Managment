622cfa6ce2df117be0e280860bc097cca8eda149
Bander Jumah <bj@banders-mbp.lan>
2019-10-19 23:25:34 +0300

    40

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $data = Company::all();
+
+        return view('companies.index', compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('companies.create');
+
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function batch()
+    {
+        return view('companies.batch');
+
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->save($request, -1);
+        return redirect('/companies');
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Company $company)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Company $t)
+    {
+
+        return view('company.edit', compact('t','t'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Company $company)
+    {
+        $this->save($request, $company->id);
+        return redirect('/companies');
+    }
+    public function saveBatch(Request $request)
+    {
+        $data = $request['data'];
+
+        $lines = explode("\n", $data);
+
+        foreach ($lines as $d)
+        {
+
+            $tokens = explode(";", $d);
+            $company = new Company();
+            $company->englishName = $tokens[0];
+            $company->arabicName = $tokens[1];
+            $company->push();
+
+        }
+
+        redirect('/companies');
+
+    }
+    public function save(Request $request, $id)
+    {
+        if($request['batch'] == 1)
+        {
+            return $this->saveBatch($request);
+        }
+        $company = new Company();
+        if($id !== -1)
+        {
+            $company = Company::find($id);
+        }
+        $company->englishName = $request['englishName'];
+        $company->arabicName = $request['arabicName'];
+
+        $company->push();
+
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Company  $company
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Company $company)
+    {
+        $company->delete = true;
+        $company->push();
+        redirect('/companies');
+
+        //
+    }
+}
---
+++ database/migrations/2019_10_19_193233_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('englishName')->nullable();
+            $table->string('arabicName')->nullable();
+
+            $table->boolean('delete')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
