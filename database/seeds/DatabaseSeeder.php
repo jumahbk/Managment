1e933d046ba082b78143404b5455447b67448695
Developer <developer@Banders-Mac-mini.local>
2019-11-03 15:19:07 +0300

    shit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(CompaniesTableSeeder::class);
+        $this->call(VendorsTableSeeder::class);
+        $this->call(UnitsTableSeeder::class);
+        $this->call(BranchsTableSeeder::class);
+        $this->call(WarehousesTableSeeder::class);
+        $this->call(ProductssTableSeeder::class);
     }
 }
