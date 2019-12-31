84e422cda6b598ff812d8d2875c51524b81ca1a2
Bander Jumah <bj@Banders-iMacm.local>
2019-12-31 18:28:14 +0300

    issue number

--- database/migrations/2019_12_21_184901_create_requests_table.php
+++ database/migrations/2019_12_21_184901_create_requests_table.php
@@ -15,6 +15,7 @@ class CreateRequestsTable extends Migration
     {
         Schema::create('requests', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('issueID');
 
             $table->unsignedBigInteger('creator')->nullable();
             $table->unsignedBigInteger('first')->nullable();
