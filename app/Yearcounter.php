caecdea46ff4250d5b017b598838d164c89ca374
Bander Jumah <bj@banders-mbp.lan>
2020-03-14 22:21:28 +0300

    counter

---
+++ app/Yearcounter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Yearcounter extends Model
+{
+    //
+}
---
+++ database/seeds/YearTableSeeder.php
@@ -0,0 +1,108 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class YearTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('yearcounters')->insert([
+            'year' => 2020,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2021,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2022,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2023,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2024,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2025,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2026,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2027,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2028,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2029,
+            'count' => 0,
+
+        ]);
+        DB::table('yearcounters')->insert([
+            'year' => 2030,
+            'count' => 0,
+
+        ]);
+
+        DB::table('yearcounters')->insert([
+            'year' => 2031,
+            'count' => 0,
+
+        ]);
+
+        DB::table('yearcounters')->insert([
+            'year' => 2032,
+            'count' => 0,
+
+        ]);
+
+        DB::table('yearcounters')->insert([
+            'year' => 2033,
+            'count' => 0,
+
+        ]);
+
+        DB::table('yearcounters')->insert([
+            'year' => 2034,
+            'count' => 0,
+
+        ]);
+
+        DB::table('yearcounters')->insert([
+            'year' => 2035,
+            'count' => 0,
+
+        ]);
+
+        DB::table('yearcounters')->insert([
+            'year' => 2036,
+            'count' => 0,
+
+        ]);
+
+    }
+}
